1dba6d4aa0ce27e8af03204008f250f1b6d4afa5
Jonathan Monsalve <jhonas2005@hotmail.com>
2025-07-09 09:11:05 -0500

    chore: add header styles

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,46 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
-        }
-        scrollPos = currentTop;
-    });
-})
+    // Incluir header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+            // Esperar un poco para asegurar que los estilos y el menú estén listos
+            setTimeout(() => {
+                let scrollPos = 0;
+                const mainNav = document.getElementById('mainNav');
+                if (mainNav) {
+                    const headerHeight = mainNav.clientHeight;
+                    window.addEventListener('scroll', function() {
+                        const currentTop = document.body.getBoundingClientRect().top * -1;
+                        if ( currentTop < scrollPos) {
+                            // Scrolling Up
+                            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                                mainNav.classList.add('is-visible');
+                                mainNav.classList.remove('is-hidden');
+                            } else {
+                                mainNav.classList.remove('is-visible', 'is-fixed', 'is-hidden');
+                            }
+                        } else {
+                            // Scrolling Down
+                            mainNav.classList.remove('is-visible');
+                            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                                mainNav.classList.add('is-fixed');
+                            }
+                            if (mainNav.classList.contains('is-fixed')) {
+                                mainNav.classList.add('is-hidden');
+                            }
+                        }
+                        scrollPos = currentTop;
+                    });
+                }
+            }, 200);
+        });
+    // Incluir footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        });
+});
