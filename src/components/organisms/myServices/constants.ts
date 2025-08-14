d012a815ece75bea5d3aa8e14e72f72130187b09
Jonathan Monsalve <cibertatan@gmail.com>
2025-08-14 16:42:30 -0500

    Update organisms index and add myServices directory

---
+++ src/components/organisms/myServices/constants.ts
@@ -0,0 +1,38 @@
+export const services = [
+    {
+      id: 1,
+      image: '/assets/img/angie-profile.png',
+      title: 'GESTIÓN DE REDES SOCIALES',
+      description: 'Planifico, publico y mantengo activa tu presencia en redes como Instagram, Facebook, TikTok y LinkedIn.'
+    },
+    {
+      id: 2,
+      image: '/assets/img/foto-quien-soy.jpg',
+      title: 'ATENCIÓN DIARIA DE MENSAJES',
+      description: 'Respondo mensajes, resuelvo dudas y fortalezco la relación con tu comunidad.'
+    },
+    {
+      id: 3,
+      image: '/assets/img/logo-angie.png',
+      title: 'REDACCIÓN DE BLOGS',
+      description: 'Creo artículos pensados para educar, atraer y posicionar tu marca con un enfoque SEO.'
+    },
+    {
+      id: 4,
+      image: '/assets/img/angie-profile.png',
+      title: 'EMAIL MARKETING',
+      description: 'Diseño y envío de correos estratégicos para fidelizar a tu audiencia, promocionar servicios y mantener una comunicación directa y profesional.'
+    },
+    {
+      id: 5,
+      image: '/assets/img/logo-angie.png',
+      title: 'PUBLICIDAD EN REDES SOCIALES',
+      description: 'Creo, configuro y optimizo campañas publicitarias para aumentar tu alcance y atraer más clientes.'
+    },
+    {
+      id: 6,
+      image: '/assets/img/foto-quien-soy.jpg',
+      title: 'ANÁLISIS DE LA COMPETENCIA',
+      description: 'Reviso lo que están haciendo otros en tu sector para encontrar oportunidades y diferenciarte.'
+    },
+  ];
