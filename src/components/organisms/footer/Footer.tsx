6fa523d98e8b775e73e09168ed95aa836a6b4013
Jonathan Monsalve <cibertatan@gmail.com>
2025-09-17 23:51:29 -0500

    refactor(footer): remove social icons from footer display
    Simplifies the footer by omitting social media icons, likely to streamline design or reduce distractions.
    This change clarifies the footer content to focus on copyright information only.

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -4,7 +4,6 @@ export function Footer() {
   return (
     <footer>
       <div className="border-t-[2px] border-[var(--color-blanco)] py-[120px] w-full" style={{paddingTop: '10px', paddingBottom: '20px'}}>
-        <SocialIcons />
         <div className="text-sm italic text-[var(--color-principal)] font-[400]">
           Copyright © Angie Galván R - Community Manager 2023
         </div>
